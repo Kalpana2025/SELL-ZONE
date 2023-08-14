1dc3f0a486073357ecf0135a89f904d29cfafcdf
Kalpana <kalpnajaif@gmail.com>
2023-08-15 01:44:23 +0530

    resolved error of undefined url

--- frontend/src/component/Home/ProductCard.js
+++ frontend/src/component/Home/ProductCard.js
@@ -2,30 +2,35 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import ReactStars from "react-rating-stars-component";
 
+const ProductCard = ({ product }) => {
+  // Check if the product object is defined
+  if (!product) {
+    return <div>Error: Product data not available.</div>;
+  }
 
+  // Check if product.images is defined and has at least one image
+  const imageUrl = product.images && product.images.length > 0 ? product.images[0].url : '';
 
+  const options = {
+    edit: false,
+    color: "#DDE6ED",
+    activeColor: "#EF2F88",
+    size: window.innerWidth < 600 ? 20 : 25,
+    value: product.ratings,
+    isHalf: true,
+  };
 
-const ProductCard = ({product}) => {
-   const options = {
-      edit:false,
-      color:"#DDE6ED",
-      activeColor:"#EF2F88",
-      size:window.innerWidth <600 ? 20 : 25,
-      value: product.ratings,
-      isHalf:true,
-   
-   };
   return (
-   <Link className="productCard" to={`/product/${product._id}`}>
-      <img src={product.images[0].url} alt={product.name}/>
+    <Link className="productCard" to={`/product/${product._id}`}>
+      <img src={imageUrl} alt={product.name} />
       <p>{product.name}</p>
       <div>
-         <ReactStars {...options} /> {" "}
-         <span>({product.numOfReviews} Reviews)</span>
+        <ReactStars {...options} /> {" "}
+ 
+        <span>({product.numOfReviews} Reviews)</span>
       </div>
       <span>{`₹${product.price}`}</span>
-   </Link>
-    
+    </Link>
   );
 };
 
